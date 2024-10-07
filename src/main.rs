c9e0ca4e39f1699bf553cae3d96fb6b4e8fa192c
Andrew Jeffery <dev@jeffas.net>
2024-10-07 14:24:23 +0100

    Switch to adjacency matrix

--- src/main.rs
+++ src/main.rs
@@ -9,85 +9,68 @@ struct Person {
 
 #[derive(Default, Debug)]
 struct Graph {
-    nodes: HashMap<u32, Person>,
-    edges: HashMap<(u32, u32), u32>,
+    nodes: HashMap<usize, Person>,
+    edges: Vec<Vec<u32>>,
 }
 
 impl Graph {
-    pub fn add_edge(&mut self, id1: u32, id2: u32, weight: u32) {
+    pub fn add_edge(&mut self, id1: usize, id2: usize, weight: u32) {
         assert!(self.nodes.contains_key(&id1));
         assert!(self.nodes.contains_key(&id2));
-        if id1 > id2 {
-            self.edges.insert((id2, id1), weight);
-        } else {
-            self.edges.insert((id1, id2), weight);
-        }
+        self.edges[id1][id2] = weight;
+        self.edges[id2][id1] = weight;
     }
 
-    pub fn add_node(&mut self, person: Person) {
-        self.nodes.insert(person.id, person);
+    pub fn add_node(&mut self, person: Person) -> usize {
+        let id = self.nodes.len();
+        self.nodes.insert(id, person);
+        for edge_row in &mut self.edges {
+            edge_row.push(0);
+        }
+        self.edges.push(vec![0; self.nodes.len()]);
+        id
     }
 
     pub fn nodes(&self) -> Vec<&Person> {
         self.nodes.values().collect()
     }
 
-    pub fn default_edges(&mut self) {
-        let keys: Vec<_> = self.nodes.keys().copied().to_owned().collect();
-        for node1 in &keys {
-            for node2 in &keys {
-                if node2 >= node1 {
-                    continue;
-                }
-                if !self.edges.contains_key(&(*node1, *node2)) {
-                    self.add_edge(*node1, *node2, 0);
-                }
-            }
-        }
+    pub fn edges_for(&self, id: usize) -> impl Iterator<Item = (usize, u32)> + '_ {
+        self.edges[id].iter().enumerate().map(|(b, w)| (b, *w))
     }
 
-    pub fn edges_for(&self, id: u32) -> impl Iterator<Item = (u32, u32)> + '_ {
-        self.edges
-            .iter()
-            .filter(move |((a, b), _)| *a == id || *b == id)
-            .map(move |((a, b), w)| if *a == id { (*b, *w) } else { (*a, *w) })
-    }
-
-    pub fn contains_node(&self, id: u32) -> bool {
-        self.nodes.contains_key(&id)
-    }
-
-    pub fn update_from_matching(&mut self, matching: &Vec<(u32, u32)>) {
+    pub fn update_from_matching(&mut self, matching: &Vec<(usize, usize)>) {
         for (a, b) in matching {
-            *self.edges.entry((*a, *b)).or_default() += 1;
+            self.edges[*a][*b] += 1;
+            self.edges[*b][*a] += 1;
         }
     }
 }
 
-fn maximal_matching(graph: &Graph) -> Vec<(u32, u32)> {
+fn maximal_matching(graph: &Graph) -> Vec<(usize, usize)> {
     let mut matchings = Vec::new();
     let mut seen = HashSet::new();
     let nodes = graph.nodes();
 
-    for node in nodes {
-        if seen.contains(&node.id) {
+    for id in 0..nodes.len() {
+        if seen.contains(&id) {
             continue;
         }
 
         let other_node = graph
-            .edges_for(node.id)
+            .edges_for(id)
             .filter(|e| !seen.contains(&e.0))
             .min_by_key(|e| e.1);
         if let Some(other_node) = other_node {
-            if node.id == other_node.0 {
+            if id == other_node.0 {
                 continue;
             }
-            if node.id < other_node.0 {
-                matchings.push((node.id, other_node.0));
+            if id < other_node.0 {
+                matchings.push((id, other_node.0));
             } else {
-                matchings.push((other_node.0, node.id));
+                matchings.push((other_node.0, id));
             }
-            seen.insert(node.id);
+            seen.insert(id);
             seen.insert(other_node.0);
         }
     }
@@ -124,9 +107,6 @@ fn main() {
     let matching1 = maximal_matching(&graph);
     dbg!(&matching1);
     graph.update_from_matching(&matching1);
-    dbg!(&graph.edges);
-
-    graph.default_edges();
     dbg!(&graph.edges);
 
     let matching2 = maximal_matching(&graph);
