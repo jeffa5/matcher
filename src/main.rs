cdfd3dd6221dad7432bf59a232f7e407b8930432
Andrew Jeffery <dev@jeffas.net>
2024-10-09 20:55:49 +0100

    Add central appstate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use axum::routing::{get, post};
 use tera::Tera;
 
+use crate::web::AppState;
+
 mod db;
 mod matching;
 mod web;
@@ -10,6 +12,8 @@ async fn main() {
     let tera = Tera::new("templates/*").unwrap();
 
     let db = db::Database::init();
+
+    let state = AppState { tera, db };
 
     let app = axum::Router::new()
         .route("/", axum::routing::get(web::root))
@@ -21,7 +25,7 @@ async fn main() {
         .route("/people", get(web::all_people))
         .route("/matches", get(web::matches))
         .route("/matches", post(web::trigger_matching))
-        .with_state((tera, db));
+        .with_state(state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Serving on http://0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
--- src/web.rs
+++ src/web.rs
@@ -10,8 +10,19 @@ use tera::{Context, Tera};
 
 use crate::{db::Database, matching::Graph};
 
-pub async fn root() -> Html<String> {
-    Html(include_str!("../templates/index.html").to_owned())
+#[derive(Clone)]
+pub struct AppState {
+    pub tera: Tera,
+    pub db: Database,
+}
+
+pub async fn root(State(state): State<AppState>) -> Html<String> {
+    Html(
+        state
+            .tera
+            .render("index.html", &Context::default())
+            .unwrap(),
+    )
 }
 
 #[derive(Debug, Deserialize)]
@@ -20,37 +31,30 @@ pub struct User {
     name: Option<String>,
 }
 
-pub async fn find_person(
-    State((_tera, db)): State<(Tera, Database)>,
-    Form(user): Form<User>,
-) -> Redirect {
-    if let Some(user) = db.find_person(&user.email) {
+pub async fn find_person(State(state): State<AppState>, Form(user): Form<User>) -> Redirect {
+    if let Some(user) = state.db.find_person(&user.email) {
         Redirect::to(&format!("/person/{}", user.id))
     } else if user.name.is_some() {
-        db.add_person(user.name.as_ref().unwrap(), &user.email);
-        let user = db.find_person(&user.email).unwrap();
+        state
+            .db
+            .add_person(user.name.as_ref().unwrap(), &user.email);
+        let user = state.db.find_person(&user.email).unwrap();
         Redirect::to(&format!("/person/{}", user.id))
     } else {
         Redirect::to(&format!("/person?email={}", user.email))
     }
 }
 
-pub async fn new_person(
-    State((tera, _db)): State<(Tera, Database)>,
-    Query(user): Query<User>,
-) -> Html<String> {
+pub async fn new_person(State(state): State<AppState>, Query(user): Query<User>) -> Html<String> {
     let mut context = Context::new();
     context.insert("email", &user.email);
     context.insert("name", &user.name);
-    Html(tera.render("add_person.html", &context).unwrap())
+    Html(state.tera.render("add_person.html", &context).unwrap())
 }
 
-pub async fn view_person(
-    State((tera, db)): State<(Tera, Database)>,
-    Path(person_id): Path<u32>,
-) -> Response {
-    if let Some(user) = db.get_person(person_id) {
-        let mut matches = db.matches_for(person_id);
+pub async fn view_person(State(state): State<AppState>, Path(person_id): Path<u32>) -> Response {
+    if let Some(user) = state.db.get_person(person_id) {
+        let mut matches = state.db.matches_for(person_id);
         matches.sort_by_key(|m| m.0);
         matches.reverse();
         let mut context = Context::new();
@@ -59,41 +63,41 @@ pub async fn view_person(
         context.insert("email", &user.email);
         context.insert("waiting", &user.waiting);
         context.insert("matches", &matches);
-        Html(tera.render("person.html", &context).unwrap()).into_response()
+        Html(state.tera.render("person.html", &context).unwrap()).into_response()
     } else {
         Redirect::to("/person").into_response()
     }
 }
 
-pub async fn all_people(State((tera, db)): State<(Tera, Database)>) -> Html<String> {
+pub async fn all_people(State(state): State<AppState>) -> Html<String> {
     let mut context = Context::new();
-    let people = db.all_people();
+    let people = state.db.all_people();
     context.insert("people", &people);
-    Html(tera.render("people.html", &context).unwrap())
+    Html(state.tera.render("people.html", &context).unwrap())
 }
 
-pub async fn matches(State((tera, db)): State<(Tera, Database)>) -> Html<String> {
+pub async fn matches(State(state): State<AppState>) -> Html<String> {
     let mut context = Context::new();
-    let match_meta = db.latest_match_meta();
-    let matches = db.latest_matches();
+    let match_meta = state.db.latest_match_meta();
+    let matches = state.db.latest_matches();
     context.insert("match_meta", &match_meta);
     context.insert("matches", &matches);
-    Html(tera.render("matches.html", &context).unwrap())
+    Html(state.tera.render("matches.html", &context).unwrap())
 }
 
-pub async fn trigger_matching(State((_tera, db)): State<(Tera, Database)>) -> Redirect {
+pub async fn trigger_matching(State(state): State<AppState>) -> Redirect {
     let mut g = Graph::default();
 
     let mut waiter_index_mapping = HashMap::new();
     let mut index_waiter_mapping = HashMap::new();
-    let waiters = db.waiters();
+    let waiters = state.db.waiters();
     for waiter in &waiters {
         let index = g.add_node(*waiter);
         waiter_index_mapping.insert(*waiter, index);
         index_waiter_mapping.insert(index, *waiter);
     }
 
-    let edges = db.edges_for(waiters);
+    let edges = state.db.edges_for(waiters);
     for (id1, id2, weight) in edges {
         g.add_edge(
             waiter_index_mapping[&id1],
@@ -104,10 +108,10 @@ pub async fn trigger_matching(State((_tera, db)): State<(Tera, Database)>) -> Re
 
     let matching = g.matching();
 
-    let generation = db.add_matching_generation();
+    let generation = state.db.add_matching_generation();
 
     for (p1, p2) in matching {
-        db.add_matching(
+        state.db.add_matching(
             index_waiter_mapping[&p1],
             p2.map(|p2| index_waiter_mapping[&p2]),
             generation,
@@ -117,10 +121,7 @@ pub async fn trigger_matching(State((_tera, db)): State<(Tera, Database)>) -> Re
     Redirect::to("/matches")
 }
 
-pub async fn add_waiter(
-    State((_tera, db)): State<(Tera, Database)>,
-    Path(person_id): Path<u32>,
-) -> Redirect {
-    db.add_waiter(person_id);
+pub async fn add_waiter(State(state): State<AppState>, Path(person_id): Path<u32>) -> Redirect {
+    state.db.add_waiter(person_id);
     Redirect::to(&format!("/person/{}", person_id))
 }
